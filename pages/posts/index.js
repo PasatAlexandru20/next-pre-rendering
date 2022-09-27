968673b342d2958b5a7cd63247d98dc8ebb5967f
Alex <palexandru8@icloud.com>
2022-09-28 01:08:14 +0300

    first commit

---
+++ pages/posts/index.js
@@ -0,0 +1,37 @@
+import Link from "next/link";
+
+function PostList({ post }) {
+  return (
+    <>
+      <h1>List of Posts</h1>
+      {post?.map((post) => {
+        return (
+          <div key={post.id}>
+            <Link href={`posts/${post.id}`} passHref>
+              <h2>
+                {post.id}
+                {post.title}
+              </h2>
+            </Link>
+            <hr />
+          </div>
+        );
+      })}
+    </>
+  );
+}
+
+export default PostList;
+
+export async function getStaticProps() {
+  const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+  const data = await response.json();
+
+  // console.log(data.slice());
+  return {
+    props: {
+      post: data,
+    },
+  };
+}
+``;
